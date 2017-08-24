e9e479916760208d2d0a25b52fac123a1d3a52c4
Asaphvs <asaph_varela@hotmail.com>
2017-08-24 11:17:37 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Login</title>
+
+</head>
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+	<title>login</title>
+</head>
+<body>
+	
+	<div class="form-login">
+
+		<form method="post" action="verifica_usuario.php">
+			
+			<input type="text"     name="login" placeholder="Digite seu login">
+			<br />
+			<input type="password" name="senha" placeholder="Digite sua senha">
+			<br />
+			<input type="submit"   name="login_form" value="Acessar">
+
+		</form>
+
+	</div>
+	
+</body>
+</html>
